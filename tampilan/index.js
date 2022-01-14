71f544dad1a1fabe2f5ca557b99f680c049277ef
Rian nazar <riannazar03@gmail.com>
2022-01-14 18:39:31 +0700

    tampilan bootcamp

---
+++ tampilan/index.js
@@ -0,0 +1,42 @@
+// grab everything we need
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+// add event listeners
+btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
+
+
+
+
+
+
+// Listen for click on the document
+document.addEventListener('click', function (event) {
+
+    //Bail if our clicked element doesn't have the class
+    if (!event.target.classList.contains('accordion-toggle')) return;
+
+    // Get the target content
+    var content = document.querySelector(event.target.hash);
+    if (!content) return;
+
+    // Prevent default link behavior
+    event.preventDefault();
+
+    // If the content is already expanded, collapse it and quit
+    if (content.classList.contains('active')) {
+        content.classList.remove('active');
+        return;
+    }
+
+    // Get all open accordion content, loop through it, and close it
+    var accordions = document.querySelectorAll('.accordion-content.active');
+    for (var i = 0; i < accordions.length; i++) {
+        accordions[i].classList.remove('active');
+    }
+
+    // Toggle our content
+    content.classList.toggle('active');
+})
