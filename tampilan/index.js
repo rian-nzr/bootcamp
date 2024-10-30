74c40c85f9b85ac1bf6ef71cc787f53ddec3b281
bekbohato <90521244+rian-nzr@users.noreply.github.com>
2024-10-30 20:17:11 +0700

    Update index.js

--- tampilan/index.js
+++ tampilan/index.js
@@ -10,18 +10,14 @@ btn.addEventListener("click", () => {
 // Listen for click on the document
 document.addEventListener('click', function (event) {
 
-    //Bail if our clicked element doesn't have the class
-    if (!event.target.classList.contains('accordion-toggle')) return;
+   if (!event.target.classList.contains('accordion-toggle')) return;
 
-    // Get the target content
-    var content = document.querySelector(event.target.hash);
+   var content = document.querySelector(event.target.hash);
     if (!content) return;
 
-    // Prevent default link behavior
     event.preventDefault();
 
-    // If the content is already expanded, collapse it and quit
-    if (content.classList.contains('active')) {
+      if (content.classList.contains('active')) {
         content.classList.remove('active');
         return;
     }
